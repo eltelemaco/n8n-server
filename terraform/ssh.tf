bd3f02b561850a9a8629a7bb4a2eb3405ef5ec78
Antonio Reynoso <telemaco.81@gmail.com>
2026-01-28 16:41:40 -0600

    chore: Remove Terraform configurations for n8n-server, including SSH keys, firewall rules, network setup, and compute resources

--- terraform/ssh.tf
+++
@@ -1,6 +0,0 @@
-# Upload the user-provided public key to Hetzner
-# The public key should be stored as a variable in HCP Terraform
-resource "hcloud_ssh_key" "n8n_key" {
-  name       = "n8n-server-key"
-  public_key = var.ssh_public_key
-}
